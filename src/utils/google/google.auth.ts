86573d3073bedaf4269ac74292d12550056063ef
come-wale-senusi <akinyemisaheedwale@gmail.com>
2026-03-13 21:19:53 +0100

    blockchain

--- src/utils/google/google.auth.ts
+++ src/utils/google/google.auth.ts
@@ -1,9 +1,23 @@
 import { google }  from "googleapis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// const auth = new google.auth.GoogleAuth({
+//     keyFile: "google-sheet-credential.json",
+//     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+//     // scopes: "https://www.googleapis.com/auth/spreadsheets",
+// });
+
+// export const sheets = google.sheets({ version: "v4", auth });
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: "google-sheet-credential.json",
-    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-    // scopes: "https://www.googleapis.com/auth/spreadsheets",
+  credentials: {
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  },
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
 export const sheets = google.sheets({ version: "v4", auth });
